48949f542b065edd8c5ed75bad80bd46b594a97a
mersadhs <96787290+mersadhs@users.noreply.github.com>
2021-12-28 19:54:16 +0330

    upload project

---
+++ pro1.js
@@ -0,0 +1,79 @@
+
+function c1()
+{
+   document.getElementById('c1').style.color="pink";    
+}
+function c1out()
+{
+    document.getElementById('c1').style.color="black";
+}
+
+function c2()
+{
+   document.getElementById('c2').style.color="pink";    
+}
+function c2out()
+{
+    document.getElementById('c2').style.color="black";
+}
+
+function c3()
+{
+   document.getElementById('c3').style.color="pink";     
+}
+
+function c3out()
+{
+    document.getElementById('c3').style.color="black";
+}
+
+function c4()
+{
+   document.getElementById('c4').style.color="pink";     
+}
+
+function c4out()
+{
+    document.getElementById('c4').style.color="black";
+}
+
+function c5()
+{
+   document.getElementById('c5').style.color="pink";     
+}
+
+function c5out()
+{
+    document.getElementById('c5').style.color="black";
+}
+
+function c6()
+{
+   document.getElementById('c6').style.color="pink";     
+}
+
+function c6out()
+{
+    document.getElementById('c6').style.color="black";
+}
+
+function c7()
+{
+   document.getElementById('c7').style.color="pink";     
+}
+
+function c7out()
+{
+    document.getElementById('c7').style.color="black";
+}
+
+function c8()
+{
+   document.getElementById('c8').style.color="red";     
+}
+
+function c8out()
+{
+    document.getElementById('c8').style.color="black";
+}
+
